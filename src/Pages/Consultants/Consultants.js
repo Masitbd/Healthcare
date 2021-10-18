b3e331c72048af2949886d98a218a8620bb9d385
Murad Sarker <sarker24@gmail.com>
2021-10-18 20:26:30 +0200

    Add the json file

---
+++ src/Pages/Consultants/Consultants.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Consultants = () => {
+  return (
+    <div>
+      <h3>This is consultants page</h3>
+    </div>
+  );
+};
+
+export default Consultants;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Consultants from "../Consultants/Consultants";
 
 const Home = () => {
   return (
     <div>
       <h3>this is home page</h3>
+      <Consultants></Consultants>
     </div>
   );
 };
